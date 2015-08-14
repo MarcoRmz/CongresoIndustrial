7a3378aafdad6b0b84c4c1a35aeba72c756b7bff
MarcoRmz <marcormz19@gmail.com>
2015-08-13 19:32:09 -0500

    attendee try 4

--- js/eventjoy.js
+++ js/eventjoy.js
@@ -155,7 +155,7 @@ var eventjoy = (function () {
 		(new _apiRequest('events/'+event_id+'/attendees', complete)).execute('GET', {'X-API-Key': _API_KEY, 'access_token': _ACCESS_TOKEN}, params);
 	};
 	ej.event_attendee = function(event_id, email, params, complete) {
-		(new _apiRequest('events/'+event_id+'/attendees/email'+email, complete)).execute('GET', {'X-API-Key': _API_KEY, 'access_token': _ACCESS_TOKEN}, params);
+		(new _apiRequest('events/'+event_id+'/attendees/email/'+email, complete)).execute('GET', {'X-API-Key': _API_KEY, 'access_token': _ACCESS_TOKEN}, params);
 	};
 	ej.order = function(order_id, complete) {
 		(new _apiRequest('orders/'+order_id, complete)).execute('GET', {'X-API-Key': _API_KEY, 'access_token': _ACCESS_TOKEN});

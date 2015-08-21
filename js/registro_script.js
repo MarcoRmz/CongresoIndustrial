6044b5a67bd201c7db8afeba198f23a2b6ae7f5e
Patricio Sanchez <partricios94@gmail.com>
2015-08-21 17:51:43 -0500

    Mattel added to Registro + Fecha Added to Talleres
    75% en Visitas (Some changes are needed)

--- js/registro_script.js
+++ js/registro_script.js
@@ -44,11 +44,11 @@ $(document).ready(function() {
                 }
                 else if (index == 3){
                     get_checked_radio('visitas', index);
-                    if (TallerValue <= 11 && VisitaValue <= 10) {
+                    if (TallerValue <= 11 && VisitaValue <= 11) {
                         alert("El Taller y la Visita seleccionados son el mismo día");
                         return false;
                     }
-                    else if (TallerValue > 11 && VisitaValue > 10) {
+                    else if (TallerValue > 11 && VisitaValue > 11) {
                         alert("El Taller y la Visita seleccionados son el mismo día");
                         return false;
                     }
@@ -56,6 +56,114 @@ $(document).ready(function() {
                         document.getElementById("codigoTaller").innerHTML = "Taller" + TallerValue + Encrypt_Taller;
                         document.getElementById("codigoVisita").innerHTML = "Visita" + VisitaValue + Encrypt_Visita;
                         return true;
+
+                        if (TallerValue <= 6){
+                            document.getElementById("taller-fecha").innerHTML = "Jueves 3:30 PM";
+                        }
+                        else if (TallerValue > 6 && TallerValue <= 11){
+                            document.getElementById("taller-fecha").innerHTML = "Jueves 5:00 PM";
+                        }
+                        else if (TallerValue > 11 && TallerValue <= 16){
+                            document.getElementById("taller-fecha").innerHTML = "Viernes 3:30 PM";
+                        }
+                        else if (TallerValue > 16){
+                            document.getElementById("taller-fecha").innerHTML = "Viernes 5:00 PM";
+                        }
+
+                        switch(VisitaValue){
+                            case 1:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 2:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 2:00 PM";    
+                            }
+                            break;
+                            case 3:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 4:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 5:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 6:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 7:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 8:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 9:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 10:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 11:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 12:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 13:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 14:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 15:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 16:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 17:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 18:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 19:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 20:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 21:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                            case 22:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 12:30 PM";
+                            }
+                            break;
+                            case 23:{
+                            document.getElementById("visita-fecha").innerHTML = "Jueves 1:30 PM";    
+                            }
+                            break;
+                        }
                     }
                     //document.getElementById("codigoTaller").innerHTML = "Taller" + TallerValue + Encrypt_Taller;
                     //document.getElementById("codigoVisita").innerHTML = "Visita" + VisitaValue + Encrypt_Visita;
